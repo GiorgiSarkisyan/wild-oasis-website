84f30aeafc0f066beb2f89796a5d14388121adf2
GiorgiSarkisyan <gios48320@gmail.com>
2024-10-11 15:21:33 +0400

    close to final

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,16 @@
+"use client";
+
+import ReservationCard from "./ReservationCard";
+import { useOptimistic } from "react";
+
+export default function ReservationList({ bookings }) {
+  useOptimistic();
+
+  return (
+    <ul className="space-y-6">
+      {bookings.map((booking) => (
+        <ReservationCard booking={booking} key={booking.id} />
+      ))}
+    </ul>
+  );
+}
--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,17 @@
-import Navigation from "../components/Navigation";
+import { auth } from "../_lib/auth";
 
-export default function Page() {
+export const metadata = {
+  title: "Guest area",
+};
+
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
   return (
     <div>
-      <h1>Your account</h1>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, {firstName}
+      </h2>
     </div>
   );
 }
